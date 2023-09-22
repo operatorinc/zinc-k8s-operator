1423ec44aa9ddbc03e2ade1cf6dfd34b1f6c427e
Carl Csaposs <carl.csaposs@canonical.com>
2023-09-22 16:57:45 +0000

    Add `ops`

--- src/charm.py
+++ src/charm.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 import logging
 
+import ops
+
+
+class ZincK8SOperatorCharm(ops.CharmBase):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.start, self._on_start)
+
+    def _on_start(self, event: ops.StartEvent):
+        logger.warning("Hello, Kubecon!")
+
+
 logger = logging.getLogger(__name__)
-logger.warning("Hello, KubeCon!")
+if __name__ == "__main__":
+    ops.main(ZincK8SOperatorCharm)
