9de2f7bfce8286de0cfc903551294f8a488c4d62
Carl Csaposs <carl.csaposs@canonical.com>
2023-09-22 16:57:52 +0000

    Add action

--- src/charm.py
+++ src/charm.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import logging
+import secrets
 
 import ops
 
@@ -8,6 +9,9 @@ class ZincCharm(ops.CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.zinc_pebble_ready, self._on_zinc_pebble_ready)
+        self.framework.observe(
+            self.on.get_admin_password_action, self._on_get_admin_password
+        )
 
     def _on_zinc_pebble_ready(self, event: ops.PebbleReadyEvent):
         """Define and start a workload using the Pebble API."""
@@ -34,11 +38,37 @@ class ZincCharm(ops.CharmBase):
                     "environment": {
                         "ZINC_DATA_PATH": "/var/lib/zincsearch",
                         "ZINC_FIRST_ADMIN_USER": "admin",
-                        "ZINC_FIRST_ADMIN_PASSWORD": "password",
+                        "ZINC_FIRST_ADMIN_PASSWORD": self._generated_password(),
                     },
                 },
             },
         }
+
+    def _on_get_admin_password(self, event: ops.ActionEvent) -> None:
+        """Return the initial generated password for the admin user as an action response."""
+        event.set_results({"admin-password": self._generated_password()})
+
+    def _generated_password(self) -> str:
+        """Report the generated admin password; generate one if it doesn't exist."""
+        # If the peer relation is not ready, just return an empty string
+        relation = self.model.get_relation("zinc-peers")
+        if not relation:
+            return ""
+
+        # If the secret already exists, grab its content and return it
+        secret_id = relation.data[self.app].get("initial-admin-password")
+        if secret_id:
+            secret = self.model.get_secret(id=secret_id)
+            return secret.peek_content()["password"]
+
+        if self.unit.is_leader():
+            content = {"password": secrets.token_urlsafe(24)}
+            secret = self.app.add_secret(content)
+            # Store the secret id in the peer relation for other units if required
+            relation.data[self.app]["initial-admin-password"] = secret.id
+            return content["password"]
+        else:
+            return ""
 
 
 logger = logging.getLogger(__name__)
